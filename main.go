5bfc5f9a8ac978df03cb01a0d36c5b0223d95778
rynfixme <qyd4v8id@gmail.com>
2024-02-25 18:00:48 +0900

    fix: `nil` ptr error

--- main.go
+++ main.go
@@ -14,16 +14,12 @@ var (
 
 	search     = app.Command("search", "Search by word.")
 	searchWord = search.Arg("word", "Word to find").Required().String()
-	// searchOutFile = search.Arg("out", "File to output")
 
 	sameIP       = app.Command("sameip", "Search by IP address, CIDR.")
 	sameIPAdress = sameIP.Arg("address", "Address or CIDR to find").Required().String()
-	// sameIPOutFile = search.Arg("out", "File to output")
 
 	subdomain       = app.Command("subdomain", "Search by Subdomain.")
 	subdomainDomain = subdomain.Arg("domain", "domain to find").Required().String()
-	// subdomainOutfile = search.Arg("out", "File to output")
-
 )
 
 func main() {
@@ -33,7 +29,8 @@ func main() {
 			prov := SearchScraperProvider[SearchResult]{}
 			c := SearchClient{searchWord, nil, &prov}
 
-			*c.Result = c.Search()
+			rerult := c.Search()
+			c.Result = &rerult
 			if c.Result == nil {
 				log.Fatalln("Search has not completed")
 			}
@@ -54,7 +51,8 @@ func main() {
 			prov := SameIPScraperProvider[SameIpResult]{}
 			c := SameIPClient{sameIPAdress, nil, &prov}
 
-			*c.Result = c.Search()
+			rerult := c.Search()
+			c.Result = &rerult
 			if c.Result == nil {
 				log.Fatalln("Search same IP has not completed")
 			}
@@ -75,7 +73,8 @@ func main() {
 			prov := SubdomainScraperProvider[SubdomainResult]{}
 			c := SubdomainClient{subdomainDomain, nil, &prov}
 
-			*c.Result = c.Search()
+			rerult := c.Search()
+			c.Result = &rerult
 			if c.Result == nil {
 				log.Fatalln("Search same IP has not completed")
 			}
